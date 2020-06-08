9be10f6e33d91f0a6d50f01df57787bc57f7c43b
AMYR <info@amyr.ir>
2020-06-08 21:45:00 +0430

    Add config file - whoami cmd - optimization

--- OvidselfBotAnimation.py
+++ OvidselfBotAnimation.py
@@ -1,51 +1,45 @@
 # 2020 - 8th - june | making animation in telegram using editing via a self-bot
 from telethon import TelegramClient, events
+from telethon.errors import SessionPasswordNeededError
 import logging
+import config, sys
+import getpass
 
-# Use your own values from my.telegram.org
-api_id = 1234
-api_hash = 'dsfoi4r3....'
+instance =  input('Enter your instance name [example: ovid]: ') if len(sys.argv) < 2 else sys.argv[1]
 # Start your app I don't know! this is for printing the error and...
-client = TelegramClient('anon', api_id, api_hash)
+client = TelegramClient(instance, config.api_id, config.api_hash)
 logging.basicConfig(
     format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
     level=logging.WARNING)
 
+def throwError(error):
+    print("[Error]: ", error)
 
 # making an event for receiving a message
 @client.on(events.NewMessage)
 async def animame(event):
-    # converting to string the message user's entered:
     cmd_message = str(event.raw_text)
+    splited_cmd_message = cmd_message.split(' ', 2)
 
-    # splitting the string just by the first two spaces:
-    # NOTE: you can change the split(' ') to anything like split('|')
-    # then your input should be <cmd_message>|<number>|<text> and so on...
-    cmd_message = cmd_message.split(' ', 2)
+    # Animation a specific message
+    if splited_cmd_message[0].lower() == 'animame' and splited_cmd_message[1].isdigit() and len(splited_cmd_message) == 3:
+        loop_time = int(splited_cmd_message[1])
+        repetition_txt = splited_cmd_message[2]
 
-    # checking if the input is based on: <animame> <number> <text>
-    # you can change the animame to anything you like
-    if cmd_message[0].lower() == 'animame' and cmd_message[1].isdigit() and len(cmd_message) == 3:
-        # just making some var to understanding better
-        loop_time = int(cmd_message[1])
-        repetition_txt = cmd_message[2]
-        # you can literally enter anything you like in respond()
-        # the text you put there is the message which will be sent but soon
-        # will be edited
         message = await event.respond(repetition_txt)
-
-        # making a loop and a loop inside it
-        # the first loop is to repeat the repetition, which is the second loop!
         for i in range(loop_time):
             for j in range(len(repetition_txt)):
-                # we cannot append ' ' to the end of a message i telegram
-                # therefor if txt[j] == ' ' we ignore it,
-                # and because we are printing with substring and
-                # it contains ' ' so in the next round,
-                # it will be added in its place
                 if repetition_txt[j] != ' ':
                     await client.edit_message(message, repetition_txt[0:j+1])
+    # Shpw myself information
+    if cmd_message == '!whoami':
+        me = await client.get_me()
+        resultText = "i'm {} with user_id: {}".format(me.first_name, me.id)
+        message = await event.respond(resultText)
 
 
-client.start()
-client.run_until_disconnected()
+try:
+    client.start()
+    client.run_until_disconnected()
+except Exception as error:
+    throwError(error)
